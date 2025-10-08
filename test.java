844e0273396ecbaa8223878a5fa3d89ee46504f3
Sai Muralidhar Raju Chennamadhavuni <chmuralidharraj@gmail.com>
2025-10-08 12:23:16 +0530

    bye

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
-import java.uitl.*;
-import java.util.Scanner;
-public static void main(String[] args){
-    System.out.println("Hello");
+import java.util.*;
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
