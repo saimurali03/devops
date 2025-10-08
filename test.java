6695008bdd1e00da4531262131867970d41ce03e
Sai Muralidhar Raju Chennamadhavuni <chmuralidharraj@gmail.com>
2025-10-08 12:17:57 +0530

    hello

--- test.java
+++ test.java
@@ -1,9 +1,5 @@
 import java.uitl.*;
 import java.util.Scanner;
 public static void main(String[] args){
-    Scanner sc=new Scanner(Sysytem.in);
-    int n=sc.nextInt();
-    for(i=0;i<n;i++){
-        System.out.println(i);
-    }
+    System.out.println("Hello");
 }
