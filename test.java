94e4133141d95a0a4dadbe739cdacbbb6eff3413
Sai Muralidhar Raju Chennamadhavuni <chmuralidharraj@gmail.com>
2025-10-08 12:13:38 +0530

    Initial commit

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.uitl.*;
+import java.util.Scanner;
+public static void main(String[] args){
+    Scanner sc=new Scanner(Sysytem.in);
+    int n=sc.nextInt();
+    for(i=0;i<n;i++){
+        System.out.println(i);
+    }
+}
